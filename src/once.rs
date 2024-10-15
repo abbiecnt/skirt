5707bf2c980f223eaeb532e4504e1e46cc86c933
KiraCoding <38864051+KiraCoding@users.noreply.github.com>
2024-10-15 15:38:07 +0200

    Initial commit

---
+++ src/once.rs
@@ -0,0 +1,32 @@
+use core::fmt::{Debug, Formatter};
+
+pub struct Once {}
+
+impl Once {
+    /// Creates a new `Once` value.
+    #[inline]
+    pub const fn new() -> Self {
+        todo!()
+    }
+
+    pub fn call_once<F>(&self, f: F)
+    where
+        F: FnOnce(),
+    {
+        todo!()
+    }
+
+    pub fn is_completed(&self) -> bool {
+        todo!()
+    }
+
+    pub fn wait(&self) {
+        todo!()
+    }
+}
+
+impl Debug for Once {
+    fn fmt(&self, f: &mut Formatter<'_>) -> core::fmt::Result {
+        f.debug_struct("Once").finish_non_exhaustive()
+    }
+}
